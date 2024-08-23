ae0cdc5ef8bedf6c8e6a94a7f6f0512e2b5c4b0f
Wang Siyuan <v-siywang@microsoft.com>
2024-08-23 19:32:34 +0800

    fix code_debug task score computing (#22)
    * fix code_debug task score computing
    
    * fix code_debug score calculation using more general regex expression
    
    * fix code_debug score calculation using more general regex expression
    
    * fix code_debug score calculation using more general regex expression
    
    * fix code_debug score calculation using more general regex expression
    
    * fix longbook choice score computation using regex
    
    * fix longbook choice score computation using regex

--- src/compute_scores.py
+++ src/compute_scores.py
@@ -181,9 +181,12 @@ def get_score_one_code_debug(pred, label, model_name: str) -> bool:
     pred = pred.strip()
     label_c = label[1]
     fn_name = label[0]
-    if pred[:2] in [f"{label_c}.", f"{label_c}:"]:
-        return True
-
+    pattern = r"\b[A-J]\b(?!.*\b[A-J]\b)"
+    match = re.search(pattern, pred)
+    if match:
+        extracted_pred = match.group(0)
+        if extracted_pred == label_c:
+            return True
     ans_prefixes = [
         "answer is:",
         # "answer is",
@@ -197,6 +200,8 @@ def get_score_one_code_debug(pred, label, model_name: str) -> bool:
         pred = pred.replace(c, " ")
     while "  " in pred:
         pred = pred.replace("  ", " ")
+    if pred.startswith(label_c) or pred.startswith(fn_name):
+        return True
     for prefix in ans_prefixes:
         idx = pred.find(prefix)
         if idx == -1:
@@ -246,6 +251,13 @@ def get_score_one_longdialogue_qa_eng(pred, label, model_name: str) -> bool:
 def get_score_one_longbook_choice_eng(pred, label, model_name: str) -> bool:
     # Just use the first letter as the prediction
     pred = pred.strip()
+    pattern = r"\b[A-D]\b(?!.*\b[A-D]\b)"
+
+    match = re.search(pattern, pred)
+    if match:
+        extracted_pred = match.group(0)
+        if extracted_pred in label:
+            return True
     if pred == "":
         return False
     if pred[0] in "ABCD":
